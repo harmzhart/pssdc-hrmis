4f203d748182c4cd9cfa670beb7ec08717e612e9
harmzhart <hamzy2000@gmail.com>
2026-01-16 12:14:21 +0100

    Connect backend to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+// Connect Database
+connectDB();
 
 const app = express();
 
